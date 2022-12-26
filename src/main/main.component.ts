8bf0253c98cd4f5bf2ceb98a9faf271afb80ec1a
Farzad <farzad.moghaddampoor@certa-systems.com>
2022-12-26 19:20:16 +0100

    Linting

--- src/main/main.component.ts
+++ src/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -8,4 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent {
   userNameValue = '';
   passwordValue = '';
+
+  // Login
+  Login() 
+  {
+    // Make HTTP request to authenticate user
+    // Set login status in application
+  }
 }
