d48e09de025fbbd46dbf6e5c43a602ca49863d88
Farzad <farzad.moghaddampoor@certa-systems.com>
2022-12-26 19:35:27 +0100

    Fix

--- src/main/main.component.ts
+++ src/main/main.component.ts
@@ -1,25 +1,36 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+// Decorator that marks a class as an Angular component and provides configuration metadata.
 @Component({
+  // The component's HTML tag name.
   selector: 'app-main',
+  // The location of the component's template file.
   templateUrl: './main.component.html',
+  // The location of the component's CSS styles.
   styleUrls: ['./main.component.css'],
 })
+
+// Main component
 export class MainComponent {
+  // Fields to store the user's name and password.
   userNameValue = '';
   passwordValue = '';
 
+  // Inject the Router service.
   constructor(private router: Router) {}
 
-  // Login
+  // Method to handle the login process.
   Login() 
   {
+    // Check if the user's name and password are both "admin".
     if (this.userNameValue === 'admin' && this.passwordValue === 'admin') {
+      // Navigate to the route for "another-page" if the login is successful.
       this.router.navigate(['/another-page']);
     } 
     else 
     {
+      // Show an alert if the login is unsuccessful.
       window.alert('Invalid username or password!');
     }
   }
